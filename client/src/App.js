7727fa63f93c0c6e7e74903050d4409259151512
Brit Sovic <bsovic@tulane.edu>
2023-03-14 10:50:44 -0400

    updated basic react project setup with file structure

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div>
+        <Home />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+          <div>
+            &copy;2023 by Brit Sovic
+          </div>
+        </footer>
+      );
+}
+
+export default Footer;
