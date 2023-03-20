1b85928f41e337b85ed149b2339a1e759cb94b7f
Brit Sovic <bsovic@tulane.edu>
2023-03-20 14:16:18 -0400

    created basic login form react component

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,14 +2,53 @@ import React, { useState } from 'react';
 
 const Login = (props) => {
     const [formState, setFormState] = useState({ username: '', password: ''});
+    
+    // Updating form
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    // Submit
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        setFormState({
+            username: '',
+            password: '',
+        });
+    };
+
     return(
-        <section>
-            <div>
+        <section class="login">
+            <div class="login-header">
                 <h1>LOGIN</h1>
             </div>
-            <div>
-                <form>
-                    <p>Choice 1</p>
+            <div class="login-form">
+                <form onSubmit={handleFormSubmit}>
+                    <input
+                    placeholder='Your username'
+                    name= 'username'
+                    type= 'text'
+                    id= 'username'
+                    value= {formState.username}
+                    onChange={handleChange}                    
+                    />
+                    <input
+                    placeholder='********'
+                    name= 'password'
+                    type= 'password'
+                    id= 'password'
+                    value= {formState.password}
+                    onChange={handleChange}                    
+                    />
+                    <button type="submit" class="login-form-btn">
+                        Login
+                    </button>
                 </form>
             </div>
         </section>
